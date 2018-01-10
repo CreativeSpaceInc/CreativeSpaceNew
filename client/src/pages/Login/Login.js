206c04b74eda7b60742e3ba83cb2855173dac527
rxi16 <rxi16@users.noreply.github.com>
2018-01-09 20:20:17 -0500

    edited Login directory

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,45 +1,77 @@
-//src/page/Login/Login.js
 import React, { Component } from "react";
+import API from "../../utils/API";
+import { DisplayMessage } from "../../components/DisplayMessage";
+import { Link } from "react-router-dom";
 import { Nav, NavLi } from "../../components/Nav";
 import { Container } from "../../components/Grid";
 import { FormBtn, FormDiv, Input } from "../../components/Form";
+import "./Login.css";
 
 class Login extends Component {
-
-  constructor(props) {
-    super(props);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  // Setting the initial values of this.state.username and this.state.password
+  state = {
+    username: "",
+    password: "",
+    artist: "5a34a9d1afaabc1820db0b32"
   };
-
-
-  handleFormSubmit = event => {
-    var obj = this;
+  // handle any changes to the input fields
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+  successMessage = () => {
+    this.setState({ 
+      message: 'Success! You have logged in. You may visit your `{<Link>profile</Link>}`.',
+      className: "success"
+    });
+  };
+  failMessage = () => {
+    this.setState({
+      message: '* Registration failed. Please try again.',
+      className: "danger"
+    });
+  };
+  // When the form is submitted, prevent the default event and alert the username and password
+  handleLoginSubmit = event => {
     event.preventDefault();
-    if(null === obj.props.validateLoginForm()){
-      alert('All fields are required.');
-      return;
+    if (this.state.username && this.state.password) {
+      API.getArtist({
+        username: this.state.username,
+        password: this.state.password
+      })
+      .then(this.successMessage())
+      .catch(err => console.log(err))
     } else {
-      this.props.loginArtist();
+      this.failMessage()
     }
+    //this.setState({ username: "", password: "" });
   };
 
   render() {
     return (
       <Container>
       <Nav>
-          <NavLi>Home</NavLi>
-          <NavLi>About</NavLi>
-          <NavLi>My profile</NavLi>
-          <NavLi>Sign up</NavLi>
+          <NavLi href="/home">Home</NavLi>
+          <NavLi href="/about">About</NavLi>
+          <NavLi href="/search">Search</NavLi>
+          <NavLi href="/signup">Sign up</NavLi> 
       </Nav>
-      <form>
-        <h1 className="my-4"><i className="fa fa-sign-in"></i> Log in</h1>
+      <h1 className="my-4"><i class="fa fa-sign-in"></i> Log in</h1>
+      <form className="card-background">
+        <DisplayMessage 
+          value={this.state.message}
+          onChange={this.handleInputChange}
+          className={this.state.className}
+          name="message">{this.state.message}
+        </DisplayMessage> 
         <FormDiv>
           <Input
             placeholder="Username"
             name="username"
-            value={this.props.username}
-            onChange={this.props.handleInputChange}
+            value={this.state.username}
+            onChange={this.handleInputChange}
           />
         </FormDiv>
         <FormDiv>
@@ -47,12 +79,12 @@ class Login extends Component {
             type="password"
             placeholder="Password"
             name="password"
-            value={this.props.password}
-            onChange={this.props.handleInputChange}
+            value={this.state.password}
+            onChange={this.handleInputChange}
           />
         </FormDiv>
-        <FormBtn
-          onClick={this.handleFormSubmit}>Log in
+        <FormBtn 
+          onClick={this.handleLoginSubmit}>Log in
         </FormBtn>
       </form>
       <hr />
